5a7b112686240210113a9aa30bf78f59e48a575e
alexfuentes <fuentes.alex@gmail.com>
2019-11-06 02:18:38 -0300

    updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,55 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
+    <div v-html="renderer" >
   </div>
 </template>
 
 <script>
-import * as d3 from 'three'
+import * as THREE from 'three'
 
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+data () {
+    return {
+     isShow:false,
+     camera:null,
+     scene:null,
+     geometry:null,
+     material:null,
+     renderer:null,
+    }
+  },
+  mounted: function() {
+
+  },methods: {
+     onInit: function(){
+      this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+      this.camera.position.z = 1;
+ 
+      this.scene = new THREE.Scene();
+ 
+      var geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+      var material = new THREE.MeshNormalMaterial();
+ 
+      var mesh = new THREE.Mesh( geometry, material );
+      this.scene.add( mesh );
+ 
+      this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+      this.renderer.setSize( window.innerWidth, window.innerHeight );
+      
+     },
+     onRender: function(){
+
+     }
+  } // end methods
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
