5f2c1206bfbad736387e39a6c5db0995310de72a
alexfuentes <fuentes.alex@gmail.com>
2019-11-06 04:02:07 -0300

    updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,14 +32,14 @@ data () {
   this.width = this.$el.offsetWidth;
 	this.height = this.$el.offsetHeight;
 	this.aspect = this.width / this.height;
-	this.camera = new Three.PerspectiveCamera( 45, this.aspect, 1, 1024 );
+	this.camera = new Three.PerspectiveCamera(65, this.aspect, 1, 1024 );
 	this.camera.position.set( this.zoom, this.zoom, this.zoom );
 	this.camera.up.set( 0, 0, 1 );
   this.camera.lookAt( new Three.Vector3( 0, 0, 0 ) );
   this.scene = new Three.Scene();
   this.geometry = new Three.BoxGeometry(1, 1, 1);
   this.material = new Three.MeshBasicMaterial({
-            color: 0xff0000 
+            color: 0xff0000
         });
   this.cube = new Three.Mesh(this.geometry, this.material);
   this.scene.add(this.cube);
@@ -51,10 +51,10 @@ data () {
 	this.renderer.setPixelRatio( window.devicePixelRatio );
   this.renderer.setSize( this.width, this.height );
   
-  this.cube.rotation.x += 0.1;
-  this.cube.rotation.y += 0.1;
+  this.cube.rotation.x += 0.3;
+  this.cube.rotation.y += 0.3;
   this.renderer.render(this.scene, this.camera);
-  
+
   },methods: {
 
   } // end methods
