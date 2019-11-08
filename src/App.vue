35eefa1fe5e8e930ea911b2ee89989e73d7f2842
alexfuentes <fuentes.alex@gmail.com>
2019-11-08 01:55:29 -0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <a-scene embedded arjs>
-      <a-marker preset="hiro">
-          <a-box position='0 0.5 0' material='color: yellow;'></a-box>
-      </a-marker>
-      <a-entity camera></a-entity>
+  <a-scene embedded arjs>
+  	<a-marker preset="hiro">
+            <a-box position='0 0.5 0' material='color: yellow;'></a-box>
+  	</a-marker>
+  	<a-entity camera></a-entity>
     </a-scene>
   </div>
 </template>
@@ -20,12 +20,5 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 0px;
-  overflow: hidden;
 }
 </style>
