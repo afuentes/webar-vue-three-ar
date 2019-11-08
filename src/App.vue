83336d201f9f83071079cc3a28253fc82c43819b
alexfuentes <fuentes.alex@gmail.com>
2019-11-08 00:21:11 -0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
-    <a-scene embedded arjs>
-      <a-marker preset="hiro">
-          <a-box position='0 0.5 0' material='color: yellow;'></a-box>
-      </a-marker>
-      <a-entity camera></a-entity>
-    </a-scene>
+                <a-scene arjs='sourceType: webcam; debugUIEnabled: true'>
+                    <a-anchor hit-testing-enabled="true">
+                        <a-entity position='0 0 0' scale='0.05 0.05 0.05' rotation='200 0 0' obj-model='obj: url(models/dog.obj); mtl: url(models/dog.mtl)'></a-entity>
+                        {/*<a-box position='0 0 0.5' material='opacity: 0.7; color: yellow;'></a-box>*/}
+                    </a-anchor>
+                    <a-camera-static preset="hiro" />
+                </a-scene>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.config.ignoredElements = [
+  'a-scene',
+  'a-entity',
+  'a-camera',
+  'a-box',
+  'a-marker',
+  'a-anchor',
+  'a-camera-static'
+]
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
