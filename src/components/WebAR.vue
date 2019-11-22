8b25d671e8c7b8ca096f852e08204d8615b53c38
alexfuentes <fuentes.alex@gmail.com>
2019-11-22 16:18:53 -0300

    updated

--- src/components/WebAR.vue
+++ src/components/WebAR.vue
@@ -1,6 +1,8 @@
 <template>
    <div>
-
+       <button>cam</button>
+       <video autoplay></video>
+       <div>{{msg_status}}</div>
    </div>
 </template>
 
@@ -15,14 +17,51 @@ export default {
   },
   data () {
     return {
-      message : null,
-      modalOpen: false
+      msg_status: '',
+      constraints    : null,
     }
   },
+  mounted: function () {
+    navigator.mediaDevices.enumerateDevices()
+             .then(this.getCamBack)
+             .catch(this.handlerError);
+    navigator.mediaDevices.getUserMedia(
+          this.constraints, 
+          this.successCallback, 
+          this.errorCallback);
+  },
  methods: {
-  search: function() {
-     this.$router.push({ path: '/search', query: { data: this.message } })
-  }
+  getCamBack: function(devices) {
+    let devices= devices.filter( v => (v.kind=="videoinput"))
+    let lastDevice= devices[devices.length-1];
+        devices= devices.filter( v => (v.label.indexOf("back")>0))
+    let device= null;
+			if( devices.length > 0 ){
+				device= devices[devices.length-1];
+			} else{
+				device= lastDevice;
+			} // find back cam
+				
+			if( !device ){
+                this.msg_status = 'No devices found !!'
+				return;
+			}
+			
+			this.constraints =
+			{
+				audio: false, 
+				video: {
+					deviceId: { ideal: device.deviceId },
+					width: { ideal: window.innerWidth },
+					height: { ideal: window.innerHeight }
+				}
+			};
+
+  },
+  handlerError: function(err){
+      this.msg_status = 'Error problems:'+err
+  },
+
   } // end methods
 }
 </script> 
