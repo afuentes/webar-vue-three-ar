a5dfcd911ba7107083843c5e94703f2e4ad7f751
alexfuentes <fuentes.alex@gmail.com>
2019-11-22 15:45:16 -0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
-  <a-scene embedded arjs>
-  	<a-marker preset="hiro">
-            <a-box position='0 0.5 0' material='color: yellow;'></a-box>
-  	</a-marker>
-  	<a-entity camera></a-entity>
-    </a-scene>
+    <a-scene>
   </div>
 </template>
 
---
+++ src/components/WebAR.vue
@@ -0,0 +1,28 @@
+<template>
+   <div>
+
+   </div>
+</template>
+
+<script>
+
+export default {
+  name: 'WebAR',
+  components: {
+    
+  },
+  props: {
+  },
+  data () {
+    return {
+      message : null,
+      modalOpen: false
+    }
+  },
+ methods: {
+  search: function() {
+     this.$router.push({ path: '/search', query: { data: this.message } })
+  }
+  } // end methods
+}
+</script> 
